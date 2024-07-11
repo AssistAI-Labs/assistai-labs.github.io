2946cec3497b07983efb96513e89c7381f2aea14
Harshit Aggarwal <67017828+harshit433@users.noreply.github.com>
2024-07-11 11:45:19 +0530

    sitemap.xml added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import generateSitemap from 'vite-ssg-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),Vue()],
+  ssgOptions: {
+    onFinished(){generateSitemap({hostname : 'https://assistai-labs.github.io'})},
+  }
 })
